d5a4d3c4ae95d6f6c2ea23e2afea3f9d20c21d37
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-06-29 14:32:40 +0300

    C# 50/100

---
+++ Inheritance/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    
+    public class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+        
+        public Vehicle(double fuel, int horsePower)
+        {
+            Fuel = fuel;
+            HorsePower = horsePower;
+           // FuelConsumption = DefaultFuelConsumption;
+        }
+
+        public double Fuel { get; set; }
+        public int HorsePower { get; set; }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= kilometers * FuelConsumption;
+        }
+
+    }
+}
