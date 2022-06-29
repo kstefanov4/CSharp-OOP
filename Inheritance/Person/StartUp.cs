f54528009c06b21b9e7be8ba8f51874e48d8e3fa
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-06-29 11:25:14 +0300

    C#

---
+++ Inheritance/Person/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            if (age > 0 && age <= 15)
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+
+            }
+            else if (age > 15)
+            {
+                Person person = new Person(name, age);
+                Console.WriteLine(person);
+            }
+            
+
+        }
+    }
+}
