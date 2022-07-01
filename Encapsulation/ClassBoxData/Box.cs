dc1d80ccd93b4c10f42d7f594170f0cfbe1404ce
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-07-02 01:25:34 +0300

    C#

---
+++ Encapsulation/ClassBoxData/Box.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double lenght;
+        private double width;
+        private double height;
+
+        public Box(double lenght, double width, double height)
+        {
+            Lenght = lenght;
+            Width = width;
+            Height = height;
+        }
+
+        public double Lenght
+        {
+            get 
+            { 
+                return lenght; 
+            }
+            set 
+            {
+                if (ValidateInputValue("Lenght",value))
+                {
+                    lenght = value;
+                }
+                
+            }
+        }
+
+        public double Width
+        {
+            get 
+            { 
+                return width; 
+            }
+            set 
+            {
+                if (ValidateInputValue("Width", value))
+                {
+                    width = value;
+                }
+                
+            }
+        }
+
+        public double Height
+        {
+            get 
+            { 
+                return height; 
+            }
+            set 
+            {
+                if (ValidateInputValue("Height", value))
+                {
+                    height = value;
+                }
+                 
+            }
+        }
+/*
+        Volume = lwh
+        Lateral Surface Area = 2lh + 2wh
+        Surface Area = 2lw + 2lh + 2wh
+*/
+        public double SurfaceArea()
+        {
+            return 2 * Lenght * Width + 2 * Lenght * Height + 2 * Width * Height; 
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * Lenght * Height + 2 * Width * Height;
+        }
+
+        public double Volume()
+        {
+            return Lenght * Height * Width;
+        }
+        private bool ValidateInputValue(string propertyName, double value)
+        {
+            if (value <= 0 )
+            {
+                throw new ArgumentException($"{propertyName} cannot be zero or negative.");
+            }
+            return true;
+        }
+
+    }
+}
