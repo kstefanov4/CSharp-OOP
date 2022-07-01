0900278e6e6253969b9e8b3864df3ec35aae5998
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-07-02 00:49:33 +0300

    C#

---
+++ Encapsulation/Team/Team.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+        private string name;
+        private readonly List<Person> firstTeam;
+        private readonly List<Person> secondTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+            this.firstTeam = new List<Person>();
+            this.secondTeam = new List<Person>();
+        }
+
+        public string Name
+        {
+            get 
+            { 
+                return name; 
+            }
+            set 
+            { 
+                name = value; 
+            }
+        }
+
+        public IReadOnlyCollection<Person> FirstTeam
+        {
+            get
+            {
+                return this.firstTeam;
+            }
+            
+        }
+        public IReadOnlyCollection<Person> SecondTeam
+        {
+            get
+            {
+                return this.secondTeam;
+            }
+            
+        }
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                secondTeam.Add(person);
+            }
+        }
+
+    }
+}
