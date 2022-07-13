fc49106cc1d31c9d74cc59aa11d4c534a8f19ca0
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2022-07-13 10:43:27 +0300

    Add files via upload

---
+++ Polymorphism/Raiding/BaseHero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Raiding
+{
+    public abstract class BaseHero
+    {
+
+        protected BaseHero(string name, int power)
+        {
+            this.Name = name;
+            this.Power = power;
+        }
+
+        public string Name { get; }
+        public int Power { get;  }
+
+        public abstract string CastAbility();
+        
+    }
+}
---
+++ Polymorphism/Raiding/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Raiding
+{
+    public class Program
+    {
+        private static object hero;
+
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            List<BaseHero> heroes = new List<BaseHero>();
+
+            while(heroes.Count != num)
+            {
+                string name = Console.ReadLine();
+                string type = Console.ReadLine();
+
+                if (type == "Druid")
+                {
+                    BaseHero druid = new Druid(name);
+                    heroes.Add(druid);
+                }
+                else if (type == "Paladin")
+                {
+                    BaseHero paladin = new Paladin(name);
+                    heroes.Add(paladin);
+                }
+                else if (type == "Rogue")
+                {
+                    BaseHero rogue = new Rogue(name);
+                    heroes.Add(rogue);
+                }
+                else if (type == "Warrior")
+                {
+                    BaseHero warrior = new Warrior(name);
+                    heroes.Add(warrior);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid hero!");
+                }
+            }
+
+            long bossPower = long.Parse(Console.ReadLine());
+
+            long heroesTotalPower = heroes.Sum(x => x.Power);
+
+            foreach (var hero in heroes)
+            {
+                Console.WriteLine(hero.CastAbility());
+                
+            }
+
+            if (heroesTotalPower >= bossPower)
+            {
+                Console.WriteLine("Victory!");
+            }
+            else
+            {
+                Console.WriteLine("Defeat...");
+            }
+        }
+    }
+}
